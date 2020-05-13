71e1cc0d61449497dd039fcb14ffb434bf807311
Jacob Leiken <jacobleiken@gmail.com>
2020-05-13 16:59:18 -0700

    There's a rectangle and it jumps

--- RegularMaria/GameScene.swift
+++ RegularMaria/GameScene.swift
@@ -11,75 +11,62 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var groundHeight : CGFloat?
+    
+    private var avatar : SKSpriteNode?
+    private var ground : SKShapeNode?
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // Create the ground
+        self.groundHeight = -self.size.height / 3
+        var groundPoints = [CGPoint(x: -self.size.width, y: self.groundHeight!),
+                            CGPoint(x: 5 * self.size.width, y: self.groundHeight!)]
+        self.ground = SKShapeNode(points: &groundPoints, count: groundPoints.count)
+        ground!.lineWidth = 5
+        ground!.physicsBody = SKPhysicsBody(edgeChainFrom: ground!.path!)
+        ground!.physicsBody!.restitution = 0.0
+        ground!.physicsBody!.friction = 1.0
+        ground!.physicsBody!.isDynamic = false
+        scene!.addChild(self.ground!)
         
-        // Create shape node to use during mouse interaction
+        // Create the character avatar
         let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        let h = (self.size.width + self.size.height) * 0.10
+        self.avatar = SKSpriteNode(color: SKColor.green, size: CGSize.init(width: w, height: h))
+        self.avatar!.position = CGPoint.init(x: 0, y: self.groundHeight!+5)
+        self.avatar!.physicsBody = SKPhysicsBody(rectangleOf: self.avatar!.size)
+        self.avatar!.physicsBody!.affectedByGravity = true
+        self.avatar!.physicsBody!.isDynamic = true
+        self.avatar!.physicsBody!.usesPreciseCollisionDetection = true
+        self.avatar!.physicsBody!.restitution = 0.0
+        scene!.addChild(self.avatar!)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if let avatar = self.avatar {
+            // if the avatar is back on the floor, let them jump again
+            let avatarBottom = avatar.position.y-(avatar.size.height/2)
+            print(avatarBottom)
+            if (avatarBottom - self.groundHeight!) < 1.5 {
+                print("it happened")
+                avatar.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 1000))
+            }
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        // mayve switch to detect a collision between the ground and character and if there's a collision and someone clicked then apply the force?
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        //for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
